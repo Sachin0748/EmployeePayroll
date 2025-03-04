73b2c8bc7d807ff1eb4cbf9bbc4d1d3c401e7c84
Sachin Kumar Thakur <kumarsachinsk456@gmail.com>
2025-03-04 15:34:39 +0530

    [Sachin] Add .Employee Payroll UC_2

--- src/main/java/com/employeepayroll/controller/EmployeeController.java
+++ src/main/java/com/employeepayroll/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.employeepayroll.controller;
 
 import com.employeepayroll.model.Employee;
+import com.employeepayroll.repo.EmployeeRepository;
 import com.employeepayroll.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,35 +14,37 @@ import java.util.Optional;
 public class EmployeeController {
 
     @Autowired
-    private EmployeeService employeeService;
+    private EmployeeRepository employeeRepository;
 
     // Add a new employee
     @PostMapping()
     public Employee addEmployee(@RequestBody Employee employee) {
-        return employeeService.addEmployee(employee);
+        return employeeRepository.save(employee);
     }
 
     // Get all employees
     @GetMapping()
     public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
+        return employeeRepository.findAll();
     }
 
     // Get employee by ID
     @GetMapping("/{id}")
     public Optional<Employee> getEmployeeById(@PathVariable Long id) {
-        return employeeService.getEmployeeById(id);
+        return employeeRepository.findById(id);
     }
 
     // Update an employee
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
-        return employeeService.updateEmployee(id, employeeDetails);
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
+        employee.setName(employeeDetails.getName());
+        employee.setDepartment(employeeDetails.getDepartment());
+        employee.setSalary(employeeDetails.getSalary());
+        return employeeRepository.save(employee);
     }
-
-    // Delete an employee
     @DeleteMapping("/{id}")
     public void deleteEmployee(@PathVariable Long id) {
-        employeeService.deleteEmployee(id);
+        employeeRepository.deleteById(id);
     }
 }
